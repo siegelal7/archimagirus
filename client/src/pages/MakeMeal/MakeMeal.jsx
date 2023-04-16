e02d0345b9cb065749a4079e58e5d21caeed3aa3
Andrew Siegel <siegelal7@gmail.com>
2023-04-16 15:20:16 -0400

    adding ingreds

--- client/src/pages/MakeMeal/MakeMeal.jsx
+++ client/src/pages/MakeMeal/MakeMeal.jsx
@@ -5,44 +5,29 @@ import GridLayout, {
   Responsive as ResponsiveGridLayout,
 } from "react-grid-layout";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 export default function MakeMeal() {
   const [ingreds, setIngreds] = useState([]);
   const [recipe, setRecipe] = useState([]);
+  const [newIngred, setNewIngred] = useState({
+    name: '',
+    type:''
+  });
 
   useEffect(() => {
     if (ingreds.length === 0) {
-      const arr = [
-        {
-          name: "test1",
-          id: "123",
-        },
-        {
-          name: "test2",
-          id: "231",
-        },
-        {
-          name: "test3",
-          id: "312",
-        },
-        {
-          name: "test4",
-          id: "321",
-        },
-        {
-          name: "test5",
-          id: "213",
-        },
-        {
-          name: "test6",
-          id: "132",
-        },
-        {
-          name: "test7",
-          id: "432",
-        },
-      ];
-      setIngreds(arr);
+      axios.get('/api/getallingredients')
+        .then(response=>{
+            if(response.status==200){
+                console.log(response);
+                setIngreds(response.data);
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    //   setIngreds(arr);
     }
   }, []);
 
@@ -65,7 +50,7 @@ export default function MakeMeal() {
     const ingredientsOnLeft = [];
     const recipeOnRight = [];
     for (let i = 0; i < e.length; i++) {
-      if (e[i]?.x === 0) {
+      if (e[i]?.x === 0) { 
         // console.log("on left");
         ingredientsOnLeft.push(e[i]);
       } else if (e[i]?.x === 1) {
@@ -84,16 +69,65 @@ export default function MakeMeal() {
     // console.log(ingreds);
   };
 
+  const handleIngredInutChange = e =>{
+        const val = e.target.value;
+        if (val != ''){
+            setNewIngred({name:val});
+        }
+  }
+
+  const handleSelectChange = e=>{
+    const valSelected = e?.target?.value;
+    if(valSelected){
+        setNewIngred({type:valSelected});
+    }
+  }
+
+  const handleNewIngredSubmit = e =>{
+    e.preventDefault();
+    // const payload = {
+    //     name: newIngred,
+    //     quantity: 1
+    // }
+    axios.post('/api/newingredient', newIngred)
+        .then(response=>{
+            console.log(response);
+            if(response.status==200){
+                // setIngreds(...ingreds,[response.data]);
+                setIngreds([...ingreds, response.data])
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+  }
+
   // const layout = [
   //     { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
   //     { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
   //   ];
   return (
     <>
+
       <Link to="/">Home</Link>
+      <form onSubmit={handleNewIngredSubmit}>
+        <label htmlFor="ingredientName">Ingredient name: </label>
+        <input name='ingredientName' style={{marginLeft:'1rem'}} value={newIngred.name} type='text' onChange={handleIngredInutChange} />
+        <label htmlFor="foodGroup">Food group: </label>
+        <select id="foodGroup" name="foodGroup" onChange={handleSelectChange}>
+            <option value="Meat">Meat</option>
+            <option value="Vegetable">Vegetable</option>
+            <option value="Fruit">Fruit</option>
+            <option value="Nut">Nut</option>
+            <option value="Drink">Drink</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Fish">Fish</option>
+        </select>
+      </form>
       {/* <div className='left'> */}
       <div className="grid">
         <h1 className="row-column">Ingredients</h1>
+        {/* <input value={newIngred} type='text' onChange={handleIngredInutChange} /> */}
         <h1 className="row-column">Recipe</h1>
       </div>
       <GridLayout
@@ -108,7 +142,7 @@ export default function MakeMeal() {
         {ingreds.length !== 0 &&
           ingreds.map((i) => {
             return (
-              <div className="padding" key={i.id}>
+              <div className="padding" key={i._id+' '+i.name}>
                 <Draggable
                   onStart={handleStart}
                   onDrag={handleDrag}
@@ -122,16 +156,8 @@ export default function MakeMeal() {
               </div>
             );
           })}
-        {/* </div> */}
-        {/* <div className='right'> */}
-        {/* <Draggable grid={[20, 900]}>
-          <div className="row-column" id="recipe">
-            MakeMeal
-          </div>
-        </Draggable> */}
-        {/* </div> */}
       </GridLayout>
-      <button onClick={handleCreateRecipe}>hmm</button>
+      <button onClick={handleCreateRecipe}>mmmmm</button>
     </>
   );
 }
--- routes/ingredientController.js
+++ routes/ingredientController.js
@@ -2,26 +2,25 @@ const router = require("express").Router();
 const db = require("../models");
 // const auth = require("../middleware/auth");
 
-// router.post("/api/name", ({ body }, res) => {
-//     db.Name.create(body)
-//       .then((person) => {
-//         res.json(person);
-//       })
-//       .catch((err) => {
-//         res.status(400).json(err);
-//       });
-//   });
+router.post("/api/newingredient", ({ body }, res) => {
+    db.Ingredient.create(body)
+      .then((ingred) => {
+        res.json(ingred);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
 
-//   router.get("/api/getallnames", (req, res) => {
-//     db.Name.find({})
-//       // .populate("kits")
-//       .then((found) => {
-//         res.json(found);
-//       })
-//       .catch((err) => {
-//         res.status(400).json(err);
-//       });
-//   });
+  router.get("/api/getallingredients", (req, res) => {
+    db.Ingredient.find({})
+      .then((found) => {
+        res.json(found);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
 
 //   router.delete('/api/deleteaperson/:id', (req,res)=>{
 //     db.Name.findByIdAndDelete(req.params.id)
